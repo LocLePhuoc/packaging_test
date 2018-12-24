fedbee4888d61d9f8b6e42927c1b35b0c45fa7b2
Le Phuoc Loc <loclp@vng.com.vn>
2018-12-24 11:54:59 +0700

    Change setup.py and add new package

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup
-
-setup(name='lppackage',
+setup(name='packaging_test',
       version='0.1',
       description='The funniest joke in the world',
       author='F2L',
       license='Zalo',
-      packages=['lppackage'],
+      packages=['lppackage', 'package_2'],
       dependency_links=['https://github.com/LocLePhuoc/packaging_test.git'],
       zip_safe=False)
