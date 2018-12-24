c9d4f357525393633e4914b0673fcad63acfa805
Le Phuoc Loc <loclp@vng.com.vn>
2018-12-24 11:59:20 +0700

    Change setup.py: remove a package

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 setup(name='packaging_test',
-      version='0.1',
+      version='0.2',
       description='The funniest joke in the world',
       author='F2L',
       license='Zalo',
-      packages=['lppackage', 'package_2'],
+      packages=['lppackage'],
       dependency_links=['https://github.com/LocLePhuoc/packaging_test.git'],
       zip_safe=False)
