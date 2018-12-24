a8e5d48b0fc749ac823c832b31889abaccb6c7b1
Le Phuoc Loc <loclp@vng.com.vn>
2018-12-24 11:20:39 +0700

    First commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='lppackage',
+      version='0.1',
+      description='The funniest joke in the world',
+      author='F2L',
+      license='Zalo',
+      packages=['lppackage'],
+      zip_safe=False)
